3811f1565e474ff96f3674e7ad170f32bb095155
museeoff <musee.hyper@gmail.com>
2024-06-26 01:00:59 +0700

    Product page finished

--- frontend/src/pages/Food/FoodPage.js
+++ frontend/src/pages/Food/FoodPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-//import classes from './foodPage.moudule.css'
-import { useParams } from 'react-router-dom';
+import classes from './foodPage.module.css'
+import { Link, useParams } from 'react-router-dom';
 import { getById } from '../../services/foodService';
 export default function FoodPage() {
   const [filter, setFilter] = useState({});
@@ -10,6 +10,27 @@ export default function FoodPage() {
     getById(id).then(setFilter);
   },[id]);
   return (
-    <div>FoodPage</div>
+    <>
+    { filter && <div className={classes.container}>
+        <img className={classes.image}
+        src={`/filters/${filter.imageUrl}`}
+        alt={filter.p_name}
+        />
+
+        <div className={classes.details}>
+          <div className={classes.header}>
+            <span className={classes.name}>{filter.p_name}
+
+            </span>
+          </div>
+          <div className={classes.code}>รหัสสินค้า {filter.p_code}</div>
+          <Link to={`/`}>
+          <div className={classes.content}>
+            Back to Catalog
+          </div>
+          </Link>
+        </div>
+      </div>}
+    </>
   )
 }
