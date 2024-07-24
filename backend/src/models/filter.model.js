eb47d531f126e6e786d72ec7c9b024eeb3e2f411
Museey <musee.hyper@gmail.com>
2024-07-24 10:42:17 +0700

    imageUrl req = false

--- backend/src/models/filter.model.js
+++ backend/src/models/filter.model.js
@@ -4,7 +4,7 @@ export const FilterSchema = new Schema(
   {
     p_code: { type: String, required: true },
     p_name: { type: String, required: true },
-    imageUrl: { type: String, required: true },
+    imageUrl: { type: String, required: false },
     
   },
   {
